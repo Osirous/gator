8b393ba4cd351340d1cc132b15431a60868708aa
osirous <cowboy6534@hotmail.com>
2024-12-09 21:36:18 -0600

    Add functionality to create feeds and handle CLI commands

--- gator/main.go
+++ gator/main.go
@@ -1,19 +1,269 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"encoding/xml"
 	"fmt"
+	"html"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"time"
 
-	"github.com/Osirous/Gator/internal/config"
+	"github.com/Osirous/gator/internal/config"
+	"github.com/Osirous/gator/internal/database"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
-const configFileName = ".gatorconfig.json"
+type state struct {
+	db     *database.Queries
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("Error reading config: %v", err)
+		log.Fatal(err)
 	}
 
-	fmt.Fprintln(cfg)
+	db, err := sql.Open("postgres", cfg.DbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
+	s := &state{
+		config: &cfg,
+		db:     dbQueries,
+	}
+
+	cmds := &commands{}
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerGetUsers)
+	cmds.register("agg", handlerAgg)
+	cmds.register("addfeed", handlerFeed)
+
+	if len(os.Args) < 2 {
+		log.Fatal("not enough arugments")
+	}
+	// os.Args[0] is the program name, [1] is login, [2] is username
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+
+	err = cmds.run(s, cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handlerFeed(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		return fmt.Errorf("please provide both a name and a URL to continue")
+	}
+
+	user, err := s.db.GetUser(context.Background(), s.config.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("unable to retreive current user: %v", err)
+	}
+
+	return addFeed(s, user.ID, cmd.args[0], cmd.args[1])
+}
+
+func addFeed(s *state, userID uuid.UUID, name string, url string) error {
+	feedParams := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       url,
+		UserID:    uuid.NullUUID{UUID: userID, Valid: true},
+	}
+
+	_, err := s.db.CreateFeed(context.Background(), feedParams)
+	if err != nil {
+		return fmt.Errorf("error creating feed: %v", err)
+	}
+
+	fmt.Printf("Feed '%s' with URL '%s' has been created!\n", name, url)
+
+	return nil
+}
+
+func handlerAgg(s *state, cmd command) error {
+	ctx := context.Background()
+	feed, err := fetchFeed(ctx, "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v\n", feed)
+	return nil
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	client := &http.Client{}
+
+	req.Header.Add("User-Agent", "gator")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	feed := &RSSFeed{}
+
+	read, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = xml.Unmarshal(read, feed)
+	if err != nil {
+		return nil, err
+	}
+
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+
+	for i := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(feed.Channel.Item[i].Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
+	}
+
+	return feed, nil
+}
+
+func handlerGetUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("error retreiving all of the users from the database: %v", err)
+	}
+
+	for _, user := range users {
+		if user == s.config.CurrentUserName {
+			fmt.Printf("* %s (current)\n", user)
+		} else {
+			fmt.Printf("* %s\n", user)
+		}
+	}
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.Reset(context.Background())
+	if err != nil {
+		return fmt.Errorf("error destroying the user table in the database: %v", err)
+	}
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("please provide a username to continue")
+	}
+
+	userParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+
+	user, err := s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			if pqErr.Code == "23505" {
+				return fmt.Errorf("user with that name already exists")
+			}
+		}
+		return err
+	}
+
+	err = s.config.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("error setting user: %v", err)
+	}
+
+	fmt.Printf("Username %s has been created!\n", user.Name)
+	fmt.Printf("Debug info: %+v\n", user)
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("username required")
+	}
+
+	username := cmd.args[0]
+
+	_, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return fmt.Errorf("user does not exist: %v", err)
+	}
+
+	err = s.config.SetUser(username)
+	if err != nil {
+		return fmt.Errorf("error setting user: %v", err)
+	}
+
+	fmt.Printf("User set to %s\n", username)
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	if c.handlers == nil {
+		c.handlers = make(map[string]func(*state, command) error)
+	}
+	c.handlers[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlers[cmd.name]
+
+	if !ok {
+		return fmt.Errorf("command does not exist: %s", cmd.name)
+	}
+
+	return handler(s, cmd)
 }
